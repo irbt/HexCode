b2e100df5d34bf69f81750b84980de35221c22f0
Emmanuel Marchal <81331382+irbt@users.noreply.github.com>
2022-12-08 14:47:08 +0100

    v1.0

--- Program.cs
+++ Program.cs
@@ -1,8 +1,18 @@
-﻿namespace Program 
-{public class Exercise1
+﻿using System.Text.RegularExpressions;
+namespace Program 
+//Write a C# Sharp program to check whether a given string is a valid Hex code or not. Return true if the said string is a valid code otherwise false
+{public class HexCode
 {
+    
+    public static bool IsHex(string s){
+        
+        //var pattern = "[#][A-Z a-z 0-9] {6}\b";
+        //Regex rg = new Regex (pattern);
+        
+        return Regex.IsMatch(s,@"[#][A-Za-z0-9]{6}\b");
+    }
     public static void Main( )
     {
-
+        Console.WriteLine (IsHex("#CD5C5C"));
     }
 }}
